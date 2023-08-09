b9c9326efacf679b7f8f226e6da491e11f7d7c63
Iamneo <techadmin@iamneo.ai>
2023-08-09 14:14:03 +0000

    Backup Commit

---
+++ springapp/src/main/java/controller/CaalculateBMI.java
@@ -0,0 +1,39 @@
+package controller;
+
+public class CalculateBMI
+ {
+
+
+		public static double calculate(double height, double weight)
+		 {
+            DeimalFormat df=new DecimalFormt("0.00");
+		  double bmi = weight / (height * height);
+          bmi=df.format(bmi*10000);
+		  return bmi;
+		}
+	  
+		public static String description(double bmi)
+		 {
+		  if (bmi < 18.5) 
+		  {
+			return "Under Weight";
+		  } 
+		  else if (bmi >= 18.5 && bmi < 24.9)
+		 {
+			return "Normal";
+		  } 
+		  else if (bmi >= 25 && bmi < 29.9)
+		   {
+			return "Over Weight";
+		  }
+		   else if (bmi >= 30 && bmi < 34.9) 
+		   {
+			return "Obese";
+		  }
+		   else
+		    {
+			return "Extremely Obese";
+		  }
+		}
+	  
+	  }
